24ec420bdbc6b162fe04fcb4e5986174f1f34cc2
Oles Slava <oles.slava23@gmail.com>
2026-09-10 16:11:22 +0300

    start practice

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h2>перший застосунок на react</h2>
+      <p></p>
+    </>
+  );
+}
+
+export default App;
